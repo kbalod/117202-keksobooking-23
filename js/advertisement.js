4e85271b1aca6514b5b8db31611462904835e2ca
kbalod <kbalod@yandex.ru>
2021-07-12 20:33:34 +0300

    Задание1

--- js/advertisement.js
+++ js/advertisement.js
@@ -1,1 +1,30 @@
-117202-keksobooking-23
+const typesMap = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+const advTemplate = document.querySelector('#card').content;
+
+export function similarAdv(adv) {
+  const advElement = advTemplate.cloneNode(true);
+  advElement.querySelector('.popup__title').textContent = adv.offer.title;
+  advElement.querySelector('.popup__text--address').textContent = adv.offer.address;
+  advElement.querySelector('.popup__text--price').textContent = `${adv.offer.price  } ₽/ночь`;
+  advElement.querySelector('.popup__type').textContent = typesMap[adv.offer.type];
+  advElement.querySelector('.popup__text--capacity').textContent = `${adv.offer.rooms  } комнаты для ${  adv.offer.guests  } гостей`;
+  advElement.querySelector('.popup__text--time').textContent = `Заезд после ${  adv.offer.checkin  }, выезд до ${  adv.offer.checkout}`;
+  const modifiers = adv.offer.features.map((feature) => `popup__feature--${feature}`);
+  advElement.querySelector('.popup__features').forEach((item)=>{
+    if (! modifiers.includes(item.classList[1])){
+      item.remove();
+    }
+  });
+  advElement.querySelector('.popup__description').textContent = adv.offer.description;
+  advElement.querySelector('.popup__photos').textContent = adv.offer.photos.map((photo) => `<img src = '${photo}'>`);
+  advElement.querySelector('.author.avatar').src = adv.author.avatar;
+  document.querySelector('#map-canvas').appendChild(advElement);
+}
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {getRandomApartment} from './data.js';
-
+import {similarAdv} from './advertisement.js';
 const object = [];
 
 for (let i = 1; i <= 10; i++) {
   const apartment = getRandomApartment(i);
   object.push(apartment);
 }
+
+similarAdv(object);
+
